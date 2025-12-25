edc2d107a0718e80c647ceb6a7b65c74ebfecd5e
Fsociety-Mrn <lisboamillen30@gmail.com>
2025-12-25 19:39:28 +0800

    feat:  add Object detections using YOLO

--- routers/camera.py
+++ routers/camera.py
@@ -3,32 +3,54 @@ from flask import (
     Response,
     jsonify
 )
-
+from ultralytics import YOLO
 import cv2
 
+
+# 20 cm distance from the object to the camera
 camera = Blueprint('camera', __name__, url_prefix="/camera/")
-camera.config["CAMERA_STATUS"] = "LOADING"
+
+model = YOLO("models/yolov8s.pt")
 
 def camera_stream(camera=None):
     while True:
         ret, frame = camera.read()
         if not ret:
-            camera.config["CAMERA_STATUS"] = "UNABLE TO LOAD THE CAMERA"
             break
         
-        camera.config["CAMERA_STATUS"] = "ONLINE"
-        
         frame = cv2.flip(frame,1)
+        results = model(frame, imgsz=320, conf=0.4)
         
-        _, frame_encoded  = cv2.imencode('.png', frame)
+        annotated = frame.copy()
+        cm_per_pixel = 1 / 37.795275591 
+        for box in results[0].boxes:
+            x1, y1, x2, y2 = box.xyxy[0].tolist()
+            width = x2 - x1
+            height = y2 - y1
+
+            real_width = width * cm_per_pixel
+            real_height = height * cm_per_pixel
+
+            cv2.rectangle(annotated, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
+
+            label = f"W:{real_width:.2f}cm H:{real_height:.2f}cm"
+            cv2.putText(
+                annotated,                # image
+                label,                    # text
+                (int(x1), int(y1) - 10),  # position (slightly above box)
+                cv2.FONT_HERSHEY_SIMPLEX, # font
+                0.5,                      # font scale
+                (0, 255, 0),              # color (green)
+                2                         # thickness
+            )
+
+        _, frame_encoded  = cv2.imencode('.png', annotated)
         yield (b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + frame_encoded.tobytes() + b'\r\n')
                 
 @camera.route('/stream')
 def stream():
-
     camera = cv2.VideoCapture(0)
-    camera.config["CAMERA_STATUS"] = "LOADING"
     return Response(camera_stream(camera=camera), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
