37db5406cc9a111aa885d10edf27dbaded55c22b
Fsociety-Mrn <lisboamillen30@gmail.com>
2025-12-30 13:48:56 +0800

    feat: add JWT authorization filter to access the ednpoints for start and status

--- routers/camera.py
+++ routers/camera.py
@@ -3,24 +3,25 @@ from flask import (
     Response,
     jsonify
 )
-from ultralytics import YOLO
-from pyzbar.pyzbar import decode
-import cv2
 from view import VideoStream
+from services import JWTManager
 
 # 20 cm distance from the object to the camera
 camera = Blueprint('camera', __name__, url_prefix="/camera/")
 vs = VideoStream()
+jwt_manager = JWTManager()
 
 @camera.route('/stream')
 def stream():
     return Response(vs.streaming(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @camera.route('/status', methods=["GET"])
+@jwt_manager.requires_access
 def status():    
     return jsonify(vs.status()), 200
 
 @camera.route('/start', methods=["PUT"])
+@jwt_manager.requires_access
 def start():    
     result = vs.start()
     return jsonify({"status": "Camera started" if result else "Camera not started"}), 200 if result else 500
