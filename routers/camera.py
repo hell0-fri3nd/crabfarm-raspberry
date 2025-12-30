89d506b368baf21f6e174c3d65f41cf6850b3440
Fsociety-Mrn <lisboamillen30@gmail.com>
2025-12-30 11:55:54 +0800

    feat: add endpoints for status and start a camera

--- routers/camera.py
+++ routers/camera.py
@@ -10,12 +10,17 @@ from template import VideoStream
 
 # 20 cm distance from the object to the camera
 camera = Blueprint('camera', __name__, url_prefix="/camera/")
+vs = VideoStream()
 
 @camera.route('/stream')
 def stream():
-    return Response(VideoStream().streaming(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
+    return Response(vs.streaming(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @camera.route('/status', methods=["GET"])
 def status():    
-    return jsonify({"status": camera.config["CAMERA_STATUS"]}), 200
+    return jsonify(vs.status()), 200
+
+@camera.route('/start', methods=["PUT"])
+def start():    
+    result = vs.start()
+    return jsonify({"status": "Camera started" if result else "Camera not started"}), 200 if result else 500
--- template/video_stream.py
+++ template/video_stream.py
@@ -1,36 +1,39 @@
 import cv2
 import threading
+import cv2
+
 from ultralytics import YOLO
 from pyzbar.pyzbar import decode
-import cv2
-from time import sleep
+
+
 class VideoStream:
     def __init__(self):
         self.camera = cv2.VideoCapture(0)
         if not self.camera.isOpened():
-            raise RuntimeError("Camera not accessible"    )
+            raise RuntimeError("Camera not accessible")
         
         self.model = YOLO("models/yolov8n.pt")
         
         self.latest_frame    = None
         self.processed_frame = None
-        self.stopped         = False
+        self.stopped         = True
         self.lock            = threading.Lock()
         
-        threading.Thread(target=self.__capture_frames, daemon=True).start()
-        threading.Thread(target=self.__run_detections, daemon=True).start()
+        self.real_width      = 0
+        self.real_height     = 0
+        self.extracted_data  = ""
     
     def __box_area(self,frame,data,box, rgb):
         x, y, w, h = box
         cv2.rectangle(frame, (x, y), (w,h),rgb, 2)
-        label = f"Data: {data}"
+        label = f"{data}"
         cv2.putText(
             frame,                    # image
             label,                    # text
-            (int(x), int(y) - 10),  # position (slightly above box)
+            (int(x), int(y) - 10),    # position (slightly above box)
             cv2.FONT_HERSHEY_SIMPLEX, # font
             0.5,                      # font scale
-            rgb,            # color (green)
+            rgb,                      # color (green)
             2                         # thickness
         )
         
@@ -39,61 +42,86 @@ class VideoStream:
     def __run_detections(self):
         
         while not self.stopped:
-      
+            
+            if self.latest_frame is None:
+                continue
+            
             results = self.model.predict(self.latest_frame, imgsz=320, conf=0.4, verbose=False)
             cm_per_pixel = 1 / 37.795275591 
             decoded_objects = decode(self.latest_frame)
             
             for box in results[0].boxes:
                 x1, y1, x2, y2 = box.xyxy[0].tolist()
-                width = x2 - x1
+                width  = x2 - x1
                 height = y2 - y1
 
-                real_width = width * cm_per_pixel
-                real_height = height * cm_per_pixel
+                self.real_width  = width * cm_per_pixel
+                self.real_height = height * cm_per_pixel
 
-                self.__box_area(self.latest_frame, f"W:{real_width:.2f}cm H:{real_height:.2f}cm", (int(x1), int(y1), int(x2), int(y2)), (0, 255, 0))
+                self.__box_area(self.latest_frame, "", (int(x1), int(y1), int(x2), int(y2)), (0, 255, 0))
             
-            for obj in decoded_objects:
-                (x, y, w, h) = obj.rect
-                self.__box_area(self.latest_frame, obj.data.decode('utf-8'), (x, y, x+ w, y + h), (255, 0, 0))
-                
-                if (obj.data.decode('utf-8') is not None):
-                    print(f"Decoded Data: {obj.data.decode('utf-8')}")
-                    self.stopped  = True
-                    self.processed_frame = self.latest_frame
+                for obj in decoded_objects:
+                    if (obj.data.decode('utf-8') is not None):
+                        (x, y, w, h) = obj.rect
+                        self.__box_area(self.latest_frame, obj.data.decode('utf-8'), (x, y, x+ w, y + h), (255, 0, 0))
+                        self.stopped  = True
+                        self.processed_frame = self.latest_frame
+                        
+                        self.extracted_data = obj.data.decode('utf-8')
                     
-            
-            # return annotated
-    
     def __capture_frames(self):
         while not self.stopped:
             ret, frame = self.camera.read()
             if ret:
                 frame = cv2.flip(frame, 1)
                 with self.lock:
-                    self.latest_frame = frame
-                
+                    self.latest_frame = frame     
+    
+    def start(self):
+        
+        try:
+            if not self.camera.isOpened():
+                self.camera = cv2.VideoCapture(0)
+
+            if self.stopped:
+                self.stopped = False
+                threading.Thread(target=self.__capture_frames, daemon=True).start()
+                threading.Thread(target=self.__run_detections, daemon=True).start()
+            
+            return self.camera.isOpened()
+        except:
+            return False
+
+          
+    def status(self):
+        data = {
+            "camera_status":  self.camera.isOpened(),
+            "width_cm":       self.real_width,
+            "height_cm":      self.real_height,
+            "extracted_data": self.extracted_data
+        }
+
+        return data
+    
     def streaming(self):
-        while True:
+        while not self.stopped:
             if self.latest_frame is None:
                 continue
             with self.lock:
                 frame = self.latest_frame.copy()
                 
             ret, buffer = cv2.imencode('.png', frame)
-            
-            if self.stopped:
-                frame = self.processed_frame
-                self.camera.release()
-                yield (b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
-                break
                 
             if not ret:
                 continue
             
             yield (b'--frame\r\n'
                     b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
-
+        
+        self.camera.release() 
+        frame = self.processed_frame.copy()
+        ret, buffer = cv2.imencode('.png', frame)
+        if ret:
+            yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
             
